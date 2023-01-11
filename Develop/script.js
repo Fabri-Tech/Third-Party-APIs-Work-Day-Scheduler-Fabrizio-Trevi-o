fd75110b46a5caf3f89712b171a75b3bd561aee7
unknown <fabriziotrevino@gmail.com>
2023-01-11 10:52:12 -0600

    working save btn

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,27 @@ const clock = document.getElementById("clock")
 function updateClock() {
     const time = dayjs().format('HH:mm:ss');
     const date = dayjs().format('MMMM D YYYY');
-    clock.innerHTML = `${date},${time},`;
+    clock.innerHTML = `${date},${time}`;
 }
 // this function will update the clock 1 second at a time 
 setInterval(updateClock, 1000); 
+
+
+// save the data to local storage when the icon "save" is clicked
+const saveButton = document.getElementsByClassName("saveBtn");
+for(let i=0; i < saveButton.length ; i++){
+    saveButton[i].addEventListener("click", function() {
+        const data = document.getElementsByClassName("description")
+        [i].value;
+        localStorage.setItem("data"+i, data);
+    });
+}
+// Retrieve the information from local storage when the web  page loads
+window.addEventListener("load", function() {
+    for(let i=0; i< saveButton.length ; i++){
+        const data = localStorage.getItem("data"+i);
+        if (data) {
+            document.getElementsByClassName("description")[i].value = data;
+        }
+    }
+});
